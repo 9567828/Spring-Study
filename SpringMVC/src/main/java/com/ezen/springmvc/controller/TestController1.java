87194048a2032f99e7fd91c7cd3a080fce47a83e
kimssi9567 <kimssi9567@gmail.com>
2024-08-07 11:16:19 +0900

    lombok 수업 진행

--- SpringMVC/src/main/java/com/ezen/springmvc/controller/TestController1.java
+++ SpringMVC/src/main/java/com/ezen/springmvc/controller/TestController1.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+// 리퀴스트매핑 -> 폴더 위치를 미리 지정해 놓는다
 @RequestMapping("/test1")
 @Controller
 public class TestController1 {
